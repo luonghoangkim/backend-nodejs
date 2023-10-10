ad055a66bc132e03e4011991e27566f13cc1b59d
luonghoangkim <akashi192002@gmail.com>
2023-10-10 14:09:05 +0700

    login user

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -1,4 +1,5 @@
 const UserService = require('../services/UserService')
+const JwtService = require('../services/JwtService')
 
 const createUser = async (req, res) => {
     try {
@@ -30,6 +31,97 @@ const createUser = async (req, res) => {
     }
 }
 
+const loginUser = async (req, res) => {
+    try {
+        const { email, password } = req.body
+        const reg = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/
+        const isCheckEmail = reg.test(email)
+        if (!email || !password) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is required'
+            })
+        } else if (!isCheckEmail) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The input is email'
+            })
+        }
+        const response = await UserService.loginUser(req.body)
+        // const { refresh_token, ...newReponse } = response
+        // res.cookie('refresh_token', refresh_token, {
+        //     httpOnly: true,
+        //     secure: false,
+        //     sameSite: 'strict',
+        //     path: '/',
+        // })
+        return res.status(200).json({response})
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const updateUser = async (req, res) => {
+    try {
+        const userId = req.params.id
+        const data = req.body
+        if (!userId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The userId is required'
+            })
+        }
+        const response = await UserService.updateUser(userId, data)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const getDetailsUser = async (req, res) => {
+    try {
+        const userId = req.params.id
+        if (!userId) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The userId is required'
+            })
+        }
+        const response = await UserService.getDetailsUser(userId)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
+const refreshToken = async (req, res) => {
+    try {
+        let token = req.headers.token.split(' ')[1]
+        if (!token) {
+            return res.status(200).json({
+                status: 'ERR',
+                message: 'The token is required'
+            })
+        }
+        const response = await JwtService.refreshTokenJwtService(token)
+        return res.status(200).json(response)
+    } catch (e) {
+        return res.status(404).json({
+            message: e
+        })
+    }
+}
+
 module.exports = {
-    createUser
+    createUser,
+    loginUser,
+    updateUser,
+    getDetailsUser,
+    refreshToken
 }
--- src/services/UserService.js
+++ src/services/UserService.js
@@ -1,5 +1,6 @@
 const User = require("../models/UserModel")
 const bcrypt = require("bcrypt")
+const { genneralAccessToken, genneralRefreshToken } = require("./JwtService")
 
 const createUser =  (newUser) => {
     return new Promise(async (resolve, reject) => {
@@ -32,7 +33,100 @@ const createUser =  (newUser) => {
     })
 }
 
+const loginUser = (userLogin) => {
+    return new Promise(async (resolve, reject) => {
+        const { email, password } = userLogin
+        try {
+            const checkUser = await User.findOne({
+                email: email
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The user is not defined'
+                })
+            }
+            const comparePassword = bcrypt.compareSync(password, checkUser.password)
+
+            if (!comparePassword) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The password is incorrect'
+                })
+            } 
+            const access_token = await genneralAccessToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            })
+            
+            const refresh_token = await genneralRefreshToken({
+                id: checkUser.id,
+                isAdmin: checkUser.isAdmin
+            }) 
+       
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',  
+                access_token,
+                refresh_token
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const updateUser = (id, data) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const checkUser = await User.findOne({
+                _id: id
+            })
+            if (checkUser === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The user is not defined'
+                })
+            }
+
+            const updatedUser = await User.findByIdAndUpdate(id, data, { new: true })
+            resolve({
+                status: 'OK',
+                message: 'SUCCESS',
+                data: updatedUser
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
+
+const getDetailsUser = (id) => {
+    return new Promise(async (resolve, reject) => {
+        try {
+            const user = await User.findOne({
+                _id: id
+            })
+            if (user === null) {
+                resolve({
+                    status: 'ERR',
+                    message: 'The user is not defined'
+                })
+            }
+            resolve({
+                status: 'OK',
+                message: 'SUCESS',
+                data: user
+            })
+        } catch (e) {
+            reject(e)
+        }
+    })
+}
 
 module.exports = {
-    createUser
+    createUser,
+    loginUser,
+    updateUser,
+    getDetailsUser
 }
